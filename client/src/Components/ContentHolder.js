c88ab63acf4ee1aed6934cff092abf3bc8ebe535
nandangrover <nandangrover.5@gmail.com>
2019-02-11 00:59:27 +0530

    Modular css

--- client/src/Components/ContentHolder.js
+++ client/src/Components/ContentHolder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import "../css/ContentHolder.css";
+import styles from "../css/ContentHolder.module.css";
 import { Container } from "reactstrap";
 // import logo from "../assets/images/logo_transparent.png";
 
@@ -12,81 +12,81 @@ class ContentHolder extends Component {
   render() {
     return (
       <Container>
-      <div className="ContentHolder">
-      <div className="article" id="1">
-      {/* <div className="titleImage"></div> */}
-      <img src={require('../assets/images/test.jpeg')} alt="titleImage" className="titleImage"></img>
-      <div className="title">DOM Selectors Explained</div>
-      <div className="articleDesc">Understanding DOM selectors to write better code in JavaScript.</div>
-      <div className="userProfile">
-      {/* <div className="profilePic"></div> */}
-      <img src={require('../assets/images/logo_transparent.png')} alt="profilePic" className="profilePic"></img>
-      <div className="userInfoWrapper">
-      <div className="userName">Nandan Grover</div>
-      <div className="date">Feb 4</div>
+      <div className={styles.ContentHolder}>
+      <div className={styles.article} id="1">
+      {/* <div className={styles.titleImage}></div> */}
+      <img src={require('../assets/images/test.jpeg')} alt={styles.titleImage} className={styles.titleImage}></img>
+      <div className={styles.title}>DOM Selectors Explained</div>
+      <div className={styles.articleDesc}>Understanding DOM selectors to write better code in JavaScript.</div>
+      <div className={styles.userProfile}>
+      {/* <div className={styles.profilePic}></div> */}
+      <img src={require('../assets/images/logo_transparent.png')} alt={styles.profilePic} className={styles.profilePic}></img>
+      <div className={styles.userInfoWrapper}>
+      <div className={styles.userName}>Nandan Grover</div>
+      <div className={styles.date}>Feb 4</div>
       </div>
       </div>
       </div>
-      <div className="article" id="2">
-      <img src={require('../assets/images/test.jpeg')} alt="titleImage" className="titleImage"></img>
-      <div className="title">DOM Selectors Explained</div>
-      <div className="articleDesc">Understanding DOM selectors to write better code in JavaScript.</div>
-      <div className="userProfile">
-      <img src={require('../assets/images/logo_transparent.png')} alt="profilePic" className="profilePic"></img>
-      <div className="userInfoWrapper">
-      <div className="userName">Nandan Grover</div>
-      <div className="date">Feb 4</div>
+      <div className={styles.article} id="2">
+      <img src={require('../assets/images/test.jpeg')} alt={styles.titleImage} className={styles.titleImage}></img>
+      <div className={styles.title}>DOM Selectors Explained</div>
+      <div className={styles.articleDesc}>Understanding DOM selectors to write better code in JavaScript.</div>
+      <div className={styles.userProfile}>
+      <img src={require('../assets/images/logo_transparent.png')} alt={styles.profilePic} className={styles.profilePic}></img>
+      <div className={styles.userInfoWrapper}>
+      <div className={styles.userName}>Nandan Grover</div>
+      <div className={styles.date}>Feb 4</div>
       </div>
       </div>
       </div>
-      <div className="article" id="3">
-      <img src={require('../assets/images/test.jpeg')} alt="titleImage" className="titleImage"></img>
-      <div className="title">DOM Selectors Explained</div>
-      <div className="articleDesc">Understanding DOM selectors to write better code in JavaScript.</div>
-      <div className="userProfile">
-      <img src={require('../assets/images/logo_transparent.png')} alt="profilePic" className="profilePic"></img>
-      <div className="userInfoWrapper">
-      <div className="userName">Nandan Grover</div>
-      <div className="date">Feb 4</div>
+      <div className={styles.article} id="3">
+      <img src={require('../assets/images/test.jpeg')} alt={styles.titleImage} className={styles.titleImage}></img>
+      <div className={styles.title}>DOM Selectors Explained</div>
+      <div className={styles.articleDesc}>Understanding DOM selectors to write better code in JavaScript.</div>
+      <div className={styles.userProfile}>
+      <img src={require('../assets/images/logo_transparent.png')} alt={styles.profilePic} className={styles.profilePic}></img>
+      <div className={styles.userInfoWrapper}>
+      <div className={styles.userName}>Nandan Grover</div>
+      <div className={styles.date}>Feb 4</div>
       </div>
       </div>
       </div>
-      <div className="horizontalLine"></div>
+      <div className={styles.horizontalLine}></div>
 
 
 
-       <div className="article" id="1">
-      {/* <div className="titleImage"></div> */}
-      <img src={require('../assets/images/test.jpeg')} alt="titleImage" className="titleImage"></img>
-      <div className="title">DOM Selectors Explained</div>
-      <div className="articleDesc">Understanding DOM selectors to write better code in JavaScript.</div>
-      <div className="userProfile">
-      <div className="profilePic"></div>
-      <div className="userName">Nandan Grover</div>
-      <div className="date">Feb 4</div>
+       <div className={styles.article} id="1">
+      {/* <div className={styles.titleImage}></div> */}
+      <img src={require('../assets/images/test.jpeg')} alt={styles.titleImage} className={styles.titleImage}></img>
+      <div className={styles.title}>DOM Selectors Explained</div>
+      <div className={styles.articleDesc}>Understanding DOM selectors to write better code in JavaScript.</div>
+      <div className={styles.userProfile}>
+      <div className={styles.profilePic}></div>
+      <div className={styles.userName}>Nandan Grover</div>
+      <div className={styles.date}>Feb 4</div>
       </div>
       </div>
-      <div className="article" id="2">
-      <img src={require('../assets/images/test.jpeg')} alt="titleImage" className="titleImage"></img>
-      <div className="title">DOM Selectors Explained</div>
-      <div className="articleDesc">Understanding DOM selectors to write better code in JavaScript.</div>
-      <div className="userProfile">
-      <div className="profilePic"></div>
-      <div className="userName">Nandan Grover</div>
-      <div className="date">Feb 4</div>
+      <div className={styles.article} id="2">
+      <img src={require('../assets/images/test.jpeg')} alt={styles.titleImage} className={styles.titleImage}></img>
+      <div className={styles.title}>DOM Selectors Explained</div>
+      <div className={styles.articleDesc}>Understanding DOM selectors to write better code in JavaScript.</div>
+      <div className={styles.userProfile}>
+      <div className={styles.profilePic}></div>
+      <div className={styles.userName}>Nandan Grover</div>
+      <div className={styles.date}>Feb 4</div>
       </div>
       </div>
-      <div className="article" id="3">
-      <img src={require('../assets/images/test.jpeg')} alt="titleImage" className="titleImage"></img>
-      <div className="title">DOM Selectors Explained</div>
-      <div className="articleDesc">Understanding DOM selectors to write better code in JavaScript.</div>
-      <div className="userProfile">
-      <div className="profilePic"></div>
-      <div className="userName">Nandan Grover</div>
-      <div className="date">Feb 4</div>
+      <div className={styles.article} id="3">
+      <img src={require('../assets/images/test.jpeg')} alt={styles.titleImage} className={styles.titleImage}></img>
+      <div className={styles.title}>DOM Selectors Explained</div>
+      <div className={styles.articleDesc}>Understanding DOM selectors to write better code in JavaScript.</div>
+      <div className={styles.userProfile}>
+      <div className={styles.profilePic}></div>
+      <div className={styles.userName}>Nandan Grover</div>
+      <div className={styles.date}>Feb 4</div>
       </div>
       </div>
-      <div className="horizontalLine"></div>
+      <div className={styles.horizontalLine}></div>
       </div>
       </Container>
     );
