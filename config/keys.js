e26b605aba28e538aa7298481f384c3cae1be012
Nandan Grover <nandangrover.5@gmail.com>
2020-09-23 02:40:11 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://nandan:nandan123@nandansblog.uwt5q.mongodb.net/nandansblog?retryWrites=true&w=majority',
-  secretOrKey: process.env.TOKEN_SECRET || 'pvpnCCZfwOF85pBjbOebZiYIDhZ3w9LZrKwBZ7152K89mPCOHtbRlmr5Z91ci4L',
-  CLOUD_NAME : 'ditsh8h8u',
-  API_KEY : '161853956253866',
-  API_SECRET : 'vKwgkrkEPPVTXTZ8NzVhy9PNWH8'
+  mongoURI: process.env.mongoURI,
+  secretOrKey: process.env.TOKEN_SECRET,
+  CLOUD_NAME : process.env.CLOUD_NAME,
+  API_KEY : process.env.API_KEY,
+  API_SECRET : process.env.API_SECRET
 }
